a1b6a2d45d3860b695a3891d53876f74c8b84566
Jerry Garcia <jerry@Jerrys-MacBook-Pro.local>
2019-04-03 15:24:27 -0700

    corrected pi computed with functions

--- BigPi.cpp
+++ BigPi.cpp
@@ -28,7 +28,7 @@ const int GROUP_SIZE = 5;                 // line grouping size
  * @param pi to be computed.
  * @return the compute time.
  */
-long compute_pi(mpf_t& pi);
+duration<double> compute_pi(mpf_t& pi);
 
 
 /**
@@ -38,139 +38,215 @@ long compute_pi(mpf_t& pi);
  * @return the compute time.
  */
 //duration<double>
-long compute_pi(mpf_class& pi);
-//print pi created with operands
-//@param string of pi
-//@param time in milliseconds
-void printPi(string str, long t);
+duration<double> compute_pi(mpf_class& pi);
+
+/**
+ * Compute the 4th root of a multiple-precision number x.
+ * @param root4 the computed 4th root.
+ * @param x the value whose root to compute.
+ * @return the 4th root.
+ */
+inline void root4(mpf_t& root4, const mpf_t& x);
+
+/**
+ * Compute the 4th root of a multiple-precision number x.
+ * @param root4 the computed 4th root.
+ * @param x the value whose root to compute.
+ * @return the 4th root.
+ */
+inline mpf_class& root4(mpf_class& root4, const mpf_class& x);
+
+/**
+ * Compute the 4th power of a multiple-precision number x.
+ * @param xxxx the computed 4th root.
+ * @param x the value whose root to compute.
+ * @return the 4th power.
+ */
+inline void power4(mpf_t& xxxx, const mpf_t& x);
+
+/**
+ * Compute the 4th power of a multiple-precision number x.
+ * @param xxxx the computed 4th root.
+ * @param x the value whose root to compute.
+ * @return the 4th power.
+ */
+inline mpf_class& power4(mpf_class& xxxx, const mpf_class& x);
+
 //print pi created with functions
 //@param cstring of pi
 //@param time in milliseconds
-void printPi(char * str, long t);
+void printPi(char * str);
 
-int main()
-{
+int main(){
     mpf_set_default_prec(BIT_COUNT*PRECISION/2);  // precision in bits
 
-    mpf_t pi2;
-    long t2 = compute_pi(pi2);
+    cout << "pi to " << PLACES << " places calculated with functions:" << endl;
 
-//    char *str = ;
-//    mp_exp_t *expptr;
-//
-//    str = mpf_get_str(str, expptr, BASE, PRECISION, pi2);
-
-    mpf_class pi3;
-    long t3 = compute_pi(pi3);
-
-    string str3;
+    mpf_t pi;
+    duration<double> elapsed_time = compute_pi(pi);
     mp_exp_t exp;
-    str3 = pi3.get_str(exp, BASE, PRECISION);
+    char *str = NULL;
 
-//    cout << "to 1000 places calculated with functions:" << endl << endl;
-//    printPi(str, t2);
+    char *s = mpf_get_str(str, &exp, BASE, PRECISION, pi);
+    printPi(s);
+    cout << "Elapsed time: " << elapsed_time.count() << " seconds" << endl;
 
-    //print out pi
-    cout << "pi to 1000 places calculated with operators:" << endl << endl;
-    printPi(str3, t3);
+    cout << endl;
+    cout << "pi to " << PLACES << " places calculated with operators:" << endl;
 
+    mpf_class pi_obj;
+    elapsed_time = compute_pi(pi_obj);
+    str = NULL;
+
+    s = mpf_get_str(str, &exp, BASE, PRECISION, pi_obj.get_mpf_t());
+    printPi(s);
+    cout << "Elapsed time: " << elapsed_time.count() << " seconds" << endl;
 
     return 0;
 }
 
-long compute_pi(mpf_t& pi){
+duration<double> compute_pi(mpf_t& pi){
 	steady_clock::time_point start_time = steady_clock::now();
 
-	mpf_t a, y, y_top, y_bot, aF, aL;
-	mpf_set_d(a, 6 - (4 * sqrt(2.0)));
-	mpf_set_d(y, sqrt(2.0) - 1);
+	mpf_t one, two, four, six, sqrt2, sqrt2x4;
+	mpf_t y, y_prev, a, a_prev, y4, yRoot4, aTerm, powers2, y2;
 
-	for(int i = 1; i <= MAX_ITERATIONS; i++){
-		//calculate y
-		mpf_pow_ui(y_top, y, 4);
-		mpf_ui_sub(y_top, 1, y_top);
-		mpf_sqrt(y_top, y_top);
-		mpf_sqrt(y_top, y_top);
-		mpf_set(y_bot, y_top);//copy y_top to y_bot
-		mpf_ui_sub(y_top, 1, y_top);//1-y_top
-		mpf_add_ui(y_bot, y_bot, 1);//1+y_bot
-		mpf_div(y, y_top, y_bot);
-		//calculate a
+	// Multiple-precision constants.
+	mpf_init(one);  mpf_set_str(one,  "1", BASE);
+	mpf_init(two);  mpf_set_str(two,  "2", BASE);
+	mpf_init(four); mpf_set_str(four, "4", BASE);
+	mpf_init(six);  mpf_set_str(six,  "6", BASE);
 
-		//calculate first part
-		mpf_add_ui(aF, y, 1);
-		mpf_pow_ui(aF, aF, 4);
-		mpf_mul(aF, a, aF);
-		//calculate second part
-		mpf_pow_ui(aL, y, 2);
-		mpf_add(aL, aL, y);
-		mpf_add_ui(aL, aL, 1);
-		int temp = pow(2, (2 * i) + 1);
-		mpf_mul(aL, aL, y);
-		mpf_mul_ui(aL, aL, temp);
-		//subtract second part from first
-		mpf_sub(a, aF, aL);
+	// The square root of 2 and four times the square root of 2.
+	mpf_init(sqrt2);   mpf_sqrt(sqrt2, two);
+	mpf_init(sqrt2x4); mpf_mul(sqrt2x4, four, sqrt2);
 
-	}
+	// Multiple-precision variables.
+	mpf_init(y);
+	mpf_init(y_prev); mpf_sub(y_prev, sqrt2, one);
+    mpf_init(a);
+    mpf_init(a_prev); mpf_sub(a_prev, six, sqrt2x4);
+    mpf_init(y4);
+    mpf_init(yRoot4);
+    mpf_init(aTerm);
+    mpf_init(powers2); mpf_set_str(powers2, "2", BASE);
+    mpf_init(y2);
+
+    for (int i = 1; i <= MAX_ITERATIONS; i++){
+    	// Compute y^4
+        power4(y4, y_prev);
+
+        mpf_t temp1, temp2;
+        mpf_init(temp1);
+        mpf_init(temp2);
+
+        // Compute the fourth root of (1 - y^4)
+        mpf_sub(temp1, one, y4);
+        root4(yRoot4, temp1);
+
+        // Compute y
+        mpf_sub(temp1, one, yRoot4);
+        mpf_add(temp2, one, yRoot4);
+        mpf_div(y, temp1, temp2);
+
+        // Compute a*(1 + y)^4
+        mpf_add(temp1, one, y);
+        power4(temp2, temp1);
+        mpf_mul(aTerm, a_prev, temp2);
+
+        // Compute 2^(2*i + 1)
+        mpf_mul(powers2, four, powers2);
+
+        // Compute y^2
+        mpf_mul(y2, y, y);
+
+        // Compute a
+        mpf_add(temp1, one, y);
+        mpf_add(temp1, temp1, y2);
+        mpf_mul(temp1, temp1, y);
+        mpf_mul(temp1, temp1, powers2);
+        mpf_sub(a, aTerm, temp1);
+
+        mpf_set(a_prev, a);
+        mpf_set(y_prev, y);
+    }
+
+    mpf_init(pi);
 
 	//inverse and assign to pi
-	mpf_ui_div(pi, 1, a);
+	mpf_div(pi, one, a);
 
 	steady_clock::time_point end_time = steady_clock::now();
-	long elapsed_time = duration_cast<milliseconds>(end_time - start_time).count();
+	duration<double> elapsed_time = end_time - start_time;
+
 	return elapsed_time;
 }
 
-long compute_pi(mpf_class& pi){
+void root4(mpf_t& root4, const mpf_t& x){
+    mpf_t root2;
+    mpf_init(root2);
+
+    mpf_sqrt(root2, x);
+    mpf_sqrt(root4, root2);
+}
+
+void power4(mpf_t& xxxx, const mpf_t& x){
+    mpf_t xx;
+    mpf_init(xx);
+
+    mpf_mul(xx, x, x);
+    mpf_mul(xxxx, xx, xx);
+}
+
+duration<double> compute_pi(mpf_class& pi){
 	steady_clock::time_point start_time = steady_clock::now();
 
-	mpf_class a, y, y_top, y_bot, aF, aL;
-	a = 6 - (4 * sqrt(2.0));
-	y = sqrt(2.0) - 1;
+	const mpf_class ONE(1), TWO(2), FOUR(4), SIX(6);
+	mpf_class y, y_prev, a, a_prev, y4, yRoot4, aTerm, powers2, temp;
 
-	for(int i = 1;i <= MAX_ITERATIONS;i++){
-		//calculate y
-		y_top = 1 - (y * y * y * y);
-		mpf_sqrt(y_top.get_mpf_t(), y_top.get_mpf_t());
-		mpf_sqrt(y_top.get_mpf_t(), y_top.get_mpf_t());
-		y_bot = y_top;
-		y_top = 1 - y_top;
-		y_bot = 1 + y_bot;
-		y = y_top / y_bot;
-		//calculate a
-		aF = y + 1;
-		aF = a * (aF * aF * aF * aF);
-		aL = y * (1 + y + (y * y));
-		aL = pow(2.0, (2 * i) + 1) * aL;
-		a = aF - aL;
+    const mpf_class SQRT2 = sqrt(TWO);
+    y_prev = SQRT2 - ONE;
+    a_prev = SIX - FOUR*SQRT2;
+    powers2 = 2;
 
-	}
-	pi = 1 / a;
+    for (int i = 1; i <= MAX_ITERATIONS; i++){
+        y4 = power4(y4, y_prev);
+        yRoot4 = root4(yRoot4, ONE - y4);
+        y = (ONE - yRoot4)/(ONE + yRoot4);
+        aTerm = a_prev*power4(temp, ONE + y);
+        powers2 = FOUR*powers2;
+        a = aTerm - powers2*y*(ONE + y + y*y);
+
+        a_prev = a;
+        y_prev = y;
+    }
+
+    pi = ONE/a;
+
 	steady_clock::time_point end_time = steady_clock::now();
-	long elapsed_time = duration_cast<milliseconds>(end_time - start_time).count();
+	duration<double> elapsed_time = end_time - start_time;
 	return elapsed_time;
 }
 
-void printPi(string str, long t){
+mpf_class& root4(mpf_class& root4, const mpf_class& x){
+    root4 = sqrt(sqrt(x));
+    return root4;
+}
+
+mpf_class& power4(mpf_class& xxxx, const mpf_class& x){
+    mpf_class xx = x*x;
+    xxxx = xx*xx;
+    return xxxx;
+}
+
+void printPi(char* str){
 	cout << str[0] << ".";
-	for(int i = 1; i < str.length(); i++){
+	for(int i = 1; i < PRECISION; i++){
 		cout << str[i];
 		if(i % 500 == 0) cout << endl << endl << "  ";
 		else if(i % LINE_SIZE == 0) cout << endl << "  ";
 		else if(i % BLOCK_SIZE == 0) cout << " ";
 
 	}
-	cout << endl << endl << "  Elapsed time: " << t << " milliseconds" <<endl;
-}
 
-void printPi(char* str, long t){
-	cout << str[0] << ".";
-		for(int i = 1; i < PRECISION; i++){
-			cout << str[i];
-			if(i % 500 == 0) cout << endl << endl << "  ";
-			else if(i % LINE_SIZE == 0) cout << endl << "  ";
-			else if(i % BLOCK_SIZE == 0) cout << " ";
-
-		}
-		cout << endl << endl << "  Elapsed time: " << t << " milliseconds" <<endl;
 }
